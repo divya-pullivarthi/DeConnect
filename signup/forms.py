9c64b31645b13a8cfd2c3f1e608c9419abd9fa06
dvp-21 <divya.pullivarthi@gmail.com>
2024-11-06 22:40:13 -0600

    Updated Login Form.

--- signup/forms.py
+++ signup/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-
+# Login Form
 class LoginForm(forms.Form):
-    email = forms.EmailField(label="Email Address")
-    #we used widget to hide the password using special characters like *.
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Email Address',
+            'class': 'form-control'
+        }),
+        label=''
+       )
+#we used widget to hide the password using special characters like *.
     password = forms.CharField(
-        widget = forms.PasswordInput(), min_length=8, max_length=20, label="Password" 
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form-control'
+        }),
+        label=''
+    # password = forms.CharField(
+    #     widget = forms.PasswordInput(), min_length=8, max_length=20, label="Password" 
     )
