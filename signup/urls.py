77acf976a5185f2f8feb5f682c47da81ed8489f5
abhijeetk783 <abhijeetk783@gmail.com>
2024-11-07 21:11:46 -0600

    Added static files, logo

--- signup/urls.py
+++ signup/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from signup.views import CustomLoginView, registration_form, register_success
+from django.contrib.auth.views import LogoutView
 
 #URL Configuration
 urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='signup:login'), name='logout'),
     path('register/', registration_form, name='registration_form'),
     path('success/', register_success, name='register_success'),
 ]
